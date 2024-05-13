d4605326126c9ec7d3316dd01ff8fab961c139d8
Iago Amorim <ogaidsa@gmail.com>
2024-05-13 15:46:59 -0300

    first  commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# ColorMemory
- Memory game with colors.
+<h1 align="center"> Colors Memory. 🧠</h1>
+<div align="center">
+  
+  <img src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white" />
+  <img src="https://img.shields.io/badge/Arduino_IDE-00979D?style=for-the-badge&logo=arduino&logoColor=white
+  " />
+  
+</div>
+
+
+# Índices 🗂
+* [Sobre](#sobre-)
+* [Fotos](#Fotos-)
+* [Como usar](#como-usar-)
+
+# Sobre ☄
+
+Um joguinho de memória com cores simples feito com Arduino.
+
+O funcionamento do jogo é simples.
+O circuito irá lhe mostrar uma sequência e você tem que repeti-la. A cada sequência completa, o jogo irá aumentar em um a quantidade de cores para a sequência. O jogo possui um sistema de record, então você terá um incentivo para sempre melhorar no jogo.
+Este código é uma reformulação de um antigo. Este projeto já foi feito por mim, mas preferi refazer o código para adicionar melhorias. 
+
+Este projeto possui uma versão física, mas, no repositório, vou deixar uma versão para o Tinkercad caso alguém se interesse. Pois, algumas funções existentes no Arduino não possuem no simulador. Além de que quando o código para a versão física estiver completa, ela vai possuir código para um componente a mais que é uma pequena tela. Essa tela não está disponível no simulador. 
+
+Obs: O código para o simulador está completo. Infelizmente a função de record não funcionará,  pois no simulador não é possível utilizar o EPROM. Então o record nunca será salvo. Sempre que a placa for desligada será perdido o record. Mas isso apenas para o código do simulador.
+
+# Fotos 📸
+<div display="inline" align="center">
+
+Pretendo colocar fotos futuramente do projeto físico
+
+</div>
+
+# Como usar 💻
+
+ 
+
+ - Se você possui experiência com Arduino, não será problema para você colocar o código na placa. Mas se não for o caso, recomendo pesquisar sobre brevemente
+
+### Materiais:
+ - 4 LED’s (uma de cada cor)
+ - 8 resistores (escolha a resistência como desejar. Vai interferir apenas no brilho das LED’s)
+ - 1 resistor para o buzzer (opcional)
+ - 5 botões
+ - 1 buzzer
+
+Siga a imagem do circuito para colocar os componentes no lugares certos:
+
+<img src="./assets/image.png" />
+
+<hr>
+<div align="center">
+
+Criado por [Iago Amorim](https://github.com/danonep2).
+
+</div>
---
+++ colorsMemory.ForTinkercad.ino
@@ -0,0 +1,192 @@
+int leds[] = {8, 9, 10, 11};
+int buttons[] = {2 , 3, 4, 5, 6};
+int Tone[] = {262, 196, 220, 247};
+
+int record;
+
+int buzzer = 7;
+bool wait = true;
+
+String currentSequence = "";
+
+byte Alto, Baixo;
+
+void setup()
+{
+  for(int i = 0; i < 4; i++){
+    pinMode(leds[i],OUTPUT);
+    pinMode(buttons[i],INPUT);
+    Serial.begin(9600);
+  }
+} // void setup()
+
+void loop(){
+ 	wait = waitLoop(wait);
+  	if (wait){
+  		return;
+  	}//if
+  
+  	currentSequence = addNumber(currentSequence);
+  
+  	showSequence();
+  
+  	wait = playerAttempt();
+    
+  	delay(500);
+} // void loop()
+
+void lightUp(int color, bool press){  
+  digitalWrite(leds[color - 1], HIGH);
+  tone(buzzer,Tone[color -1], 200);
+  delay(200);
+  
+  digitalWrite(leds[color - 1], LOW);  
+  if (!press){
+    delay(300);
+  }// if
+}
+
+void showSequence(){
+  int sequenceSize = currentSequence.length();
+  for(int i = 0; i < sequenceSize; i++){
+  	int currentColor = currentSequence[i];
+    currentColor -= 48;
+    lightUp(currentColor, false);
+  }//for
+}// void showSequence
+
+int waitButton(bool isWaittingPlayer) {
+  for(int button : buttons){
+    if(digitalRead(button)){
+      	while(digitalRead(button)){
+          if(button >= 2 && button <= 5 && !isWaittingPlayer){
+            // Liga a led e reproduz o som
+            // caso seja a vez do jogador
+          	lightUp(button -1, true);
+          }// if
+      	  delay(1);
+      	}//while
+    	return button - 1;
+    } // if
+  }// for
+  return 0;
+} // int waitButton
+
+bool waitLoop(bool isWaittingPlayer){
+  	if (!isWaittingPlayer){
+    	return isWaittingPlayer;
+    }// if
+
+  	bool isLightOn = false;
+  	for(int count = 0; count < 1001; count++){
+      	int buttonPressed = waitButton(true);
+      	
+      	if (count % 200 == 0){
+      		isLightOn = !isLightOn;
+      	}//if        
+
+      	if (buttonPressed == 5){
+      		isWaittingPlayer = false;
+          	isLightOn = false;
+          	Serial.println("Jogo inciiado");
+      	}//if
+      
+      	for(int led : leds){
+        	digitalWrite(led, isLightOn ? HIGH : LOW);
+        }//for 
+      
+      	if (!isWaittingPlayer){
+        	break;
+        }//if
+  		delay(1);
+  	}// for
+  
+	return isWaittingPlayer;
+} //bool waitLoop()
+
+
+void endMusic(){
+  delay(300);
+  tone(buzzer, 220, 300);
+  
+  delay(400);
+  tone(buzzer, 208, 300);
+  
+  delay(450);
+  tone(buzzer, 196, 300);
+  
+  delay(450);
+  tone(buzzer, 185, 1500);
+  delay(1000);
+
+}// void endMusic
+
+void breakRecord(){ 
+  
+  delay(300);
+  tone(buzzer, 185, 180);
+  
+  delay(200);
+  tone(buzzer, 233, 180);
+  
+  delay(200);
+  tone(buzzer, 277, 180);
+  
+  delay(200);
+  tone(buzzer, 740, 1500);
+  
+  delay(1000);
+    
+}// void breakRecord
+
+
+
+bool playerAttempt(){
+	String playerSequence = "";
+  	
+  	while(true){
+    	int buttonPressed = waitButton(false);
+      	if(buttonPressed == 5){
+			currentSequence = "";
+          	return true;
+        }//if
+      	
+      	if (buttonPressed == 0){
+        	continue;
+        }//if
+      
+      	playerSequence = playerSequence + buttonPressed;
+      
+      	Serial.print("Sequencia do player: ");
+      	Serial.println(playerSequence);
+      	
+      	int lastPressedIndex = playerSequence.length() - 1;
+      
+      	if(playerSequence[lastPressedIndex] != currentSequence[lastPressedIndex]){
+			int sequenceSize = currentSequence.length() -1;
+          	currentSequence = "";
+	
+	        if (sequenceSize > record){
+               	Alto = record / 256;
+              
+                breakRecord();
+              	record = sequenceSize;
+                return true;
+            }// if
+          	endMusic();
+          	delay(300);
+          	return true;
+        }//if
+      
+      	if(playerSequence.length() == currentSequence.length()){
+        	return false;
+        }//if
+      	
+    }//while
+}// bool playerAttempt()
+
+
+String addNumber(String currentList){
+  int New = rand() % 4;
+  return currentList + (New + 1);
+}// char addNumber
